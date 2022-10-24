99a0fe5af9398f9053bee76570394caec0831573
Bria White <bria.awhite@gmail.com>
2022-10-24 14:33:47 -0400

    Refactor

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ function addPlace(e) {
     e.preventDefault()
     const locationInput = e.target.children[0]
     const cityInput = e.target.children[1]
+    const imgInput = e.target.children[2]
     fetch("http://localhost:3000/places", {
         method: "POST",
         headers: {
@@ -17,7 +18,8 @@ function addPlace(e) {
         },
         body: JSON.stringify({
             location: locationInput.value,
-            city: cityInput.value
+            city: cityInput.value,
+            img: imgInput.value
         })
     })
     .then(resp => resp.json())
@@ -33,14 +35,16 @@ function fetchPlaces() {
         console.log(data)
         const places = document.querySelector("#places-container")
         places.innerHTML = showPlaces(data)
-        addPlaceEvents()
+        // addPlaceEvents()
+        const place = document.querySelectorAll(".place-delete-button")
+       place.forEach(pl => pl.addEventListener("click", deletePlace))
     })
 }
 
-function addPlaceEvents() {
-    const places = document.querySelectorAll(".place-delete-button")
-    places.forEach(pl => pl.addEventListener("click", deletePlace))
-}
+// function addPlaceEvents() {
+//     const places = document.querySelectorAll(".place-delete-button")
+//     places.forEach(pl => pl.addEventListener("click", deletePlace))
+// }
 
 function deletePlace(e) {
     e.target.parentElement.id
@@ -64,6 +68,7 @@ function showOnePlace(place) {
       <div class="place-card" id="${place.id}">
          <div class="place-frame" id="${place.id}">
             <h4 class="center-text">${place.location}, ${place.city}</h4>
+            <img src= ${place.img} height=200 width=200> 
             <button data-action="delete" class="place-delete-button">X</button>
          </div>
          <br>
@@ -75,8 +80,7 @@ function showOnePlace(place) {
 function addCreateForm() {
     const formContainer = document.getElementById("form-container");
     const form = document.createElement("form")
-    form.innerHTML = `<input id="name-input" placeholder='Place' type='text'/><input id="location-input" placeholder='City' type='text'/><input id="place-submit" value="Create Place" type='submit'/>`
+    form.innerHTML = `<input id="name-input" placeholder='Place' type='text'/><input id="location-input" placeholder='City' type='text'/><input id="img-input" placeholder="Image" type='text'/><input id="place-submit" value="Create Place" type='submit'/>`
     formContainer.append(form)
-
     form.addEventListener("submit", addPlace)
 }
